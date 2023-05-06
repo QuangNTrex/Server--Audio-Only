dfde5d6cb259975e28327a1ad6ffdef17d0e2dc3
Tien Quang <quangnt.rex@gmail.com>
2023-05-06 22:40:57 +0700

    completed

--- app.js
+++ app.js
@@ -9,19 +9,32 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/music/:videoId", (req, res, next) => {
-  const videoId = req.params.videoId;
-  console.log(videoId);
-  const url = `https://www.youtube.com/watch?v=${videoId}`;
-  const stream = ytdl(url, { filter: "audioonly", quality: "highestaudio" });
+app.use("/musics/:musicId", (req, res, next) => {
+  const musicId = req.params.musicId.split(".")[0];
+  console.log(musicId);
 
-  res.setHeader("Content-Type", "audio/mpeg");
+  if (fs.existsSync(path.join(__dirname, "musics", musicId + ".mp3")))
+    return next();
 
-  stream.pipe(res);
+  // delete after 10 minutes
+  setTimeout(
+    ((musicId) => {
+      fs.unlinkSync(path.join(__dirname, "musics", musicId + ".mp3"));
+    }).bind(null, musicId),
+    1000 * 60 * 10
+  );
 
-  stream.on("end", () => {
-    console.log("end");
+  const stream = ytdl(`https://www.youtube.com/watch?v=${musicId}`, {
+    filter: "audioonly",
+    quality: "highestaudio",
+    format: "mp3",
   });
+
+  stream.pipe(
+    fs.createWriteStream(path.join(__dirname, "musics", musicId + ".mp3"))
+  );
+
+  stream.on("end", () => next());
 });
 
 app.use("/musics", express.static(path.join(__dirname, "musics")));
