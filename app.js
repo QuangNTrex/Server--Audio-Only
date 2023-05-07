2efc8158786be1d693c437d1cd510145c85546ec
Tien Quang <quangnt.rex@gmail.com>
2023-05-07 10:21:57 +0700

    ff

--- app.js
+++ app.js
@@ -3,28 +3,42 @@ const ytdl = require("ytdl-core");
 const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
-
+const ffmpeg = require("fluent-ffmpeg");
+const ffmpegPath = require("ffmpeg-static").path;
+ffmpeg.setFfmpegPath(ffmpegPath);
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/musics/:musicId", (req, res, next) => {
-  const musicId = req.params.musicId.split(".")[0];
+async function convertToM4a(mp3FilePath, m4aFilePath) {
+  ffmpeg(mp3FilePath)
+    .format("m4a")
+    .on("error", (err) => console.error(err))
+    .on("end", () => console.log("Finished!"))
+    .save(m4aFilePath);
+  console.log("before convert");
+}
+
+app.use("/musics/:musicPath", (req, res, next) => {
+  const musicPath = req.params.musicPath;
+  const musicId = req.params.musicPath.split(".")[0];
+  const format = req.params.musicPath.split(".")[1];
   console.log(musicId);
 
   console.log("in check exist file");
-  if (fs.existsSync(path.join(__dirname, "musics", musicId + ".mp3")))
-    return next();
+  if (fs.existsSync(path.join(__dirname, "musics", musicPath))) return next();
 
   // delete after 10 minutes
   console.log("in add setTimeout");
   setTimeout(
-    ((musicId) => {
+    ((musicId, musicPath) => {
       if (fs.existsSync(path.join(__dirname, "musics", musicId + ".mp3")))
         fs.unlinkSync(path.join(__dirname, "musics", musicId + ".mp3"));
-    }).bind(null, musicId),
-    1000 * 40
+      if (fs.existsSync(path.join(__dirname, "musics", musicPath)))
+        fs.unlinkSync(path.join(__dirname, "musics", musicPath));
+    }).bind(null, musicId, musicPath),
+    1000 * 60 * 5
   );
 
   console.log("in create stream music");
@@ -42,6 +56,14 @@ app.use("/musics/:musicId", (req, res, next) => {
   console.log("check on end");
   stream.on("end", () => {
     stream.destroy();
+    if (format !== "mp3") {
+      convertToM4a(
+        path.join(__dirname, "musics", musicId + ".mp3"),
+        path.join(__dirname, "musics", musicPath)
+      );
+      console.log("after convert");
+    }
+
     console.log("in next");
     console.log(__dirname);
     console.log(
