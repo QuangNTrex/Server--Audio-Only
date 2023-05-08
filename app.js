4e5a30bafedc1bb0e672ba24fee474e0f6f3c3a9
Tien Quang <quangnt.rex@gmail.com>
2023-05-08 11:03:59 +0700

    hhh

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const ffmpeg = require("ffmpeg");
 const { exec } = require("child_process");
 const app = express();
 
+const mapCheck = new Map();
+
 app.use(cors());
 app.use(express.json());
 
-async function convertToM4a(mp3File, m4aFile, next) {
+async function convertToM4a(mp3File, m4aFile, res, startTime) {
   exec(
     `ffmpeg -i ${__dirname.replace(/\\/g, "/") + `/musics/${mp3File}`} ${
       __dirname.replace(/\\/g, "/") + `/musics/${m4aFile}`
@@ -18,22 +20,32 @@ async function convertToM4a(mp3File, m4aFile, next) {
     (err, std) => {
       console.log(err);
       console.log("completed convert");
-      next();
+      if (err)
+        res.send({
+          error: { time: (Date.now() - startTime) / 1000, message: err },
+        });
+      else
+        res.send({
+          result: {
+            time: (Date.now() - startTime) / 1000,
+            message: "converted",
+          },
+        });
     }
   );
 }
 
 app.use("/musics/:musicPath", (req, res, next) => {
+  const startTime = Date.now();
   const musicPath = req.params.musicPath;
   const musicId = req.params.musicPath.split(".")[0];
   const format = req.params.musicPath.split(".")[1];
   console.log(musicId);
 
   console.log("in check exist file");
-  if (fs.existsSync(path.join(__dirname, "musics", musicPath))) return next();
+  if (fs.existsSync(path.join(__dirname, "musics", musicPath)))
+    return res.send({ result: { message: "converted" } });
 
-  // delete after 10 minutes
-  console.log("in add setTimeout");
   setTimeout(
     ((musicId, musicPath) => {
       if (fs.existsSync(path.join(__dirname, "musics", musicId + ".mp3")))
@@ -44,13 +56,11 @@ app.use("/musics/:musicPath", (req, res, next) => {
     1000 * 60 * 5
   );
 
-  console.log("in create stream music");
   const stream = ytdl(`https://www.youtube.com/watch?v=${musicId}`, {
     filter: "audioonly",
     quality: "highestaudio",
   });
 
-  console.log("pipe");
   stream.pipe(
     fs.createWriteStream(path.join(__dirname, "musics", musicId + ".mp3"))
   );
@@ -59,20 +69,12 @@ app.use("/musics/:musicPath", (req, res, next) => {
   stream.on("end", () => {
     stream.destroy();
     if (format !== "mp3") {
-      convertToM4a(musicId + ".mp3", musicPath, next);
+      convertToM4a(musicId + ".mp3", musicPath, res, startTime);
       console.log("after convert");
     }
-
-    // console.log("in next");
-    // console.log(__dirname);
-    // console.log(
-    //   "is have file:",
-    //   fs.existsSync(path.join(__dirname, "musics", musicPath))
-    // );
   });
 });
 
-app.use("/musics", express.static(path.join(__dirname, "musics")));
 app.use("/music-folder", express.static(path.join(__dirname, "musics")));
 
 app.use("/test", (req, res, next) => {
